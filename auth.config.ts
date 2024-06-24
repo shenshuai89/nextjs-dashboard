1b543e00b2f826314d5e5c6b38c70337ebd2728c
shenshuai_dr <shen-sh@neusoft.com>
2024-06-24 15:15:09 +0800

    添加访问权限配置注释

--- auth.config.ts
+++ auth.config.ts
@@ -7,6 +7,8 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
+      // 这里添加的访问 dashboard 的权限控制，
+      // 发布上线时，需要在 settings 中添加 .env 中变量配置
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
